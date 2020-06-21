103c6bf57a6c87b5d45bdbac3f562402809832e7
Christian Stewart <christian@paral.in>
2020-06-21 16:36:25 -0700

    chore: add prettier and lint-staged to package.json
    Signed-off-by: Christian Stewart <christian@paral.in>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+client = new SoyuzClient(schema, (msg: rgraphql.IRGQLClientMessage) => {
+    if (!sender) {
+        sender = this.rpc.startCall(
+            {
+                rgqlClientMessage: msg,
+                rpcId: ipc.RPC.RPC_RGQLClientMessage
+            },
+            (rmsg: ipc.IRPCMessage) => {
+                const smsg = rmsg.rgqlServerMessage
+                if (!smsg) {
+                    return
+                }
+                client.handleMessages([smsg])
+            }
+        )
+        return
+    }
+
+    sender({
+        rgqlClientMessage: msg,
+        rpcId: ipc.RPC.RPC_RGQLClientMessage
+    })
+})
+*/
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
